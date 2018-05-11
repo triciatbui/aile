4f2a808fef3c5bb95f0f60b4e59f0e7712fa0dab
triciatbui <triciatbui@gmail.com>
2018-05-11 08:54:39 -0700

    first
    rise

---
+++ jquery.js
@@ -0,0 +1,82 @@
+// JavaScript Document
+
+// Sticky Header
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 100) {
+        $('.main_h').addClass('sticky');
+    } else {
+        $('.main_h').removeClass('sticky');
+    }
+});
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.main_h').removeClass('open-nav');
+    } else {
+        $('.main_h').addClass('open-nav');
+    }
+});
+
+$('.main_h li a').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main_h').removeClass('open-nav');
+    }
+});
+
+// navigation scroll lijepo radi materem
+$('nav a').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
+});
+
+//quote slider 
+var sentences = new Array ('Braille is knowledge and knowledge is power.','Access to communication in the widest sense is access to knowledge.','Literacy is the most basic currency of the knowledge economy.','Literacy is the road to human progress and the means through which every man, woman and child can realize his or her full potential.','Braille is the gateway to knowledge is the gateway to independence is the gateway to opportunities.');
+
+var timer = 8000;
+
+$(document).ready(function(){
+
+    var total = sentences.length - 1;
+
+    for (var i = 0; i <= total; i++) {
+
+        $('#box').append('<p class="sl" id="textBox'+i+'"></p>');
+         
+        var max = sentences[i].length - 1;
+
+        for (var j = 0; j <= max; j++) {
+            $('#textBox'+i).append('<span style="transition: ' + Math.random()*3 + 's; transition-delay: ' + Math.random() + 's;">' + sentences[i].charAt(j) + '</span>'); 
+        };
+
+    }; 
+
+    var maxBox = $('#box > p').length;
+    var r = 0;
+
+    $('#textBox' + r).addClass('active');
+
+    setInterval(function(){ 
+
+        $('#textBox' + r).removeClass('active');
+
+        r++;
+
+        if (r == maxBox) {
+            r = 0;
+        }
+   
+        setTimeout(function(){ 
+            $('#textBox' + r).addClass('active');
+        }, 2000);
+
+    }, timer);
+
+}); 
